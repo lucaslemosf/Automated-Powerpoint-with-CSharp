92742fa31f64ff2159f8940f2fd08699dde13526
Lucas Franco <lucaslemos.f@gmail.com>
2019-04-27 00:37:58 +0200

    code initiated

--- PowerpointCsharp/PowerpointCsharp/Program.cs
+++ PowerpointCsharp/PowerpointCsharp/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Microsoft.Office.Interop.PowerPoint;
+using System.IO;
+using PowerPoint = Microsoft.Office.Interop.PowerPoint;
 
 namespace PowerpointCsharp
 {
@@ -6,7 +9,19 @@ namespace PowerpointCsharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Creating an Application
+            Application myApplication = new Application();
+            //Creating a Presentation - opening a existing PowerPoint      you need to put the @
+            Presentation myPresentation = myApplication.Presentations.Open(@"C:\Users\lucaslemos\Desktop\Github\PowerPoint-CSharp\tutorial_slide.pptx");
+            //Dealing with slides
+            PowerPoint.Slides slides; //will be used as the whole collection of my presentation
+            PowerPoint._Slide slide; //will be used as my current slide being edited
+            slides = myPresentation.Slides; // (big S)
+            PowerPoint.CustomLayout customLayout = myPresentation.SlideMaster.CustomLayouts[PowerPoint.PpSlideLayout.ppLayoutText];
+            slide = slides.AddSlide(2, customLayout); //Creating a new slide, which will be second slide of my presentation
+                                                      //CustomLayout is a mandatory input
+
+
         }
     }
 }
